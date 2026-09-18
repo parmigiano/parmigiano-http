6e73f0772722eb5edf6efdce45da7279c97f3026
nenishyi <aworkprogramm@gmail.com>
2026-09-19 04:30:47 +0500

    refactor: decouple HTTP routes from RabbitMQ

--- http-service/include/routes.h
+++ http-service/include/routes.h
@@ -1,19 +1,9 @@
 #ifndef ROUTES_H
 #define ROUTES_H
 
-#include "rabbitmq.h"
-
 #include <libchttpx/libchttpx.h>
-
-typedef struct
-{
-    const char* queue;
-    rmq_handler_t handler;
-} rabbitmq_route_t;
 
 void http_routes(chttpx_serv_t* server);
 void moderation_routes(chttpx_serv_t* server);
-
-const rabbitmq_route_t* rabbitmq_routes(size_t* count);
 
 #endif
